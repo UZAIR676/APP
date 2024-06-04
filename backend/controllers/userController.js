7a04e55e8b12e7310d236692ca8a667e0e577019
uzair rana <you@example.com>
2024-06-05 00:24:19 +0500

    chore: Add updateCurrentUserProfile endpoint to userRoutes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,7 +84,49 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
 
 const getcurrentUser = asyncHandler(async(req,res)=>{
 const user =   await User.findById(req.user._id)
-console.log(user)
+if(user){
+  res.status(200).json({
+    _id : user._id,
+    username : user.username,
+    email : user.email,
+
+  })
+}
+else{
+  res.status(404)
+  throw error ("user not found")
+}
 })
 
-export { createUser ,loginUser, logoutCurrentUser ,getAllUsers,getcurrentUser};
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    
+    if (user) {
+      user.username = req.body.username || user.username;
+      user.email = req.body.email || user.email;
+
+      if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(req.body.password, salt);
+      }
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+export { createUser ,loginUser, logoutCurrentUser ,getAllUsers,getcurrentUser,updateCurrentUserProfile};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 // userRoutes.js
 import express from 'express';
-import { createUser, loginUser , logoutCurrentUser, getAllUsers,getcurrentUser} from '../controllers/userController.js';
+import { createUser, loginUser , logoutCurrentUser, getAllUsers,getcurrentUser,updateCurrentUserProfile} from '../controllers/userController.js';
 import { authenticated, authorizeAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+router.post('/', createUser);
 
 router.route('/',createUser).get(authenticated,authorizeAdmin,getAllUsers)
 
@@ -14,5 +16,5 @@ router.post('/auth', loginUser);
 
 router.post('/logout', logoutCurrentUser);
 
-router.route("/profile").get(authenticated,getcurrentUser)
+router.route("/profile").get(authenticated,getcurrentUser).get(authenticated,updateCurrentUserProfile)
 export default router;
